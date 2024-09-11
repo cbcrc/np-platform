d8ec06f70b00a8b10ee148ec98a1afbcdcfbe61f
charlescrepeau-rc <165849964+charlescrepeau-rc@users.noreply.github.com>
2024-09-11 14:00:40 -0400

    add titles writer to byline

--- client/component/people.js
+++ client/component/people.js
@@ -1,34 +1,16 @@
-import { useDatastore, useIsLive, useObject, usePersonaKey, usePersonaObject, usePersonaPreview as usePersonaPreview } from "../util/datastore";
-import { getFirebaseUser } from "../util/firebase";
+import { useDatastore, usePersonaObject, usePersonaPreview as usePersonaPreview } from "../util/datastore";
 import { Image, StyleSheet, Text, View } from "react-native";
 import { IconCircleCheck } from "./icon";
 import { UtilityText } from "./text";
-import { HoverView, Pad } from "./basics";
+import { HoverView, Pad, spacings } from "./basics";
 import { formatDate, formatMiniDate } from "./date";
-import { colorBlack, colorDisabledBackground, colorTextGrey, colorWhite } from "./color";
+import { colorBlack, colorDisabledBackground, colorTextGrey, colorWhite, colorPink } from "./color";
 import { TextButton } from "./button";
-import { gotoInstance } from "../util/navigate";
 import { useLanguage } from "./translation";
 
-
-export function ProfilePhoto({userId, type='large', photo=null, faint=false, check=false, border=false}) {
-    const persona = useObject('persona', userId);
-    const isLive = useIsLive();
-    const meKey = usePersonaKey();
-
-    if (meKey == userId && isLive) {
-        return <MyProfilePhoto type={type} photo={photo} faint={faint} check={check} border={border} />
-    } else {
-        const face = persona?.face;
-        if (face || photo || persona?.photoUrl) {
-            return <FaceImage face={face} photoUrl={photo ?? persona?.photoUrl} type={type} 
-                border={border} faint={faint} check={check} />
-        } else if (persona?.hue && persona?.name) {
-            return <LetterFace name={persona.name} hue={persona.hue} type={type} />
-        } else {
-            return <AnonymousFace faint={faint} type={type} border={border} />    
-        }
-    }
+export function ProfilePhoto({userId, type='large', photo=null, faint=false, check=false, border=false, badge=null}) {
+    return <FaceImage face={photo} photoUrl={photo} type={type} 
+                border={border} faint={faint} check={check} badge={badge} />
 }
 
 export function MyProfilePhoto({type='large', photo=null, faint=false, check=false, border=false}) {
@@ -43,8 +25,7 @@ export function MyProfilePhoto({type='large', photo=null, faint=false, check=fal
     }
 }
 
-
-export function FaceImage({face, photoUrl=null, type='small', faint=false, check=false, border=false}) {
+export function FaceImage({face, photoUrl=null, type='small', faint=false, check=false, border=false, badge=null}) {
     const sizeMap = {
         huge: 80,
         large: 40,
@@ -60,22 +41,28 @@ export function FaceImage({face, photoUrl=null, type='small', faint=false, check
     }
     const checkPad = check ? checkPadMap[type ?? 'small'] : 0;
 
-
     return <View style={{position: 'relative', alignSelf: 'flex-start'}}>
-        <Image 
+            <Image 
             style ={{
                 width: size, height: size, borderRadius: size /2, 
                 opacity: faint ? 0.5 : 1, marginRight: checkPad,
                 borderWidth: border ? 2 : 0,
                 borderColor: 'white'
-                // border: border ? '2px solid white' : null
             }}
             source={{uri: photoUrl ?? ('https://new-public-demo.web.app/faces/' + face)}} />
-    
+            {badge && <View style={{position: 'absolute', right: 0, bottom: 0}}>
+             <Image 
+                style={{
+                    width: 14, height: 14, borderRadius: 7,
+                    borderColor: 'white',
+                    backgroundColor: 'white',
+                    borderWidth: 2
+                }}
+                source={{ uri: badge }} />
+            </View>}
             {check && <View style={{position: 'absolute', right: 0, bottom: 0}}>
                 <IconCircleCheck />
-            </View>
-        }
+            </View>}
     </View>
 }
 
@@ -135,10 +122,7 @@ const FacePileStyle = StyleSheet.create({
     }
 })
 
-
-
-
-export function Byline({type='small', photoType=null, clickable=true, userId, name=null, photo=null, time, subtitleLabel, subtitleParams={}, underline=false, edited=false}) {
+export function Byline({type='small', photoType=null, clickable=true, userId, name=null, photo=null, time, subtitleLabel, subtitleParams={}, underline=false, edited=false, badge=null, tag=null}) {
     const s = BylineStyle
     const persona = usePersonaObject(userId);
     const language = useLanguage();
@@ -146,7 +130,31 @@ export function Byline({type='small', photoType=null, clickable=true, userId, na
     function onProfile() {
         datastore.gotoInstance({structureKey: 'profile', instanceKey: userId});
     }
-    if (type == 'large') {
+    if (badge && tag) { // titled writer layout
+        return <View style={s.outer}>
+            <ProfilePhoto userId={userId} photo={photo} type={'large'} badge={badge} />
+             <Pad size={spacings.xs} />
+             <View style={{ flexDirection: 'column' }}>
+                <View style={{ flexDirection: 'row' }}>
+                    {clickable ?
+                        <TextButton type='small' strong text={name} underline={underline} onPress={onProfile} />
+                    : 
+                        <UtilityText strong text={name} underline={underline} />
+                    }
+                    {time && <Pad size={6} />}
+                    {time &&                     <UtilityText color={colorTextGrey} 
+                                label={'{time}' + (edited ? ' • Edited' : '')} 
+                                formatParams={{time: formatMiniDate(time, language)}} underline={underline}/>
+                    }
+                </View>
+                {tag && <View style={s.tag}>
+                     <UtilityText label={tag} type='tiny' />
+                </View>
+                }
+        </View>
+    </View>   
+    }
+    else if (type == 'large') {
         return <View style={s.outer}>
             <ProfilePhoto userId={userId} photo={photo} type={photoType ?? 'large'} /> 
             <View style={s.right}>
@@ -162,7 +170,8 @@ export function Byline({type='small', photoType=null, clickable=true, userId, na
                 }
             </View>
         </View>
-    } else {
+    }
+    else {
         return <View style={s.smallOuter}>
             <ProfilePhoto userId={userId} photo={photo} type={photoType ?? 'small'} /> 
             <Pad size={8} />
@@ -191,6 +200,18 @@ const BylineStyle = StyleSheet.create({
     smallOuter: {
         flexDirection: 'row',
         alignItems: 'center',
+    },
+    tag: {
+        alignSelf: 'flex-start',
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderWidth: 1,
+        marginTop: 4,
+        paddingHorizontal: 8,
+        paddingVertical: 4,
+        borderRadius: 100,
+        backgroundColor: colorPink,
+        borderColor: colorPink,
     }
 })
 
@@ -210,6 +231,7 @@ export function FaceButton({selected, onPress, testID, children}) {
         </View>
     </HoverView>
 }
+
 const FaceButtonStyle = StyleSheet.create({
     ring: {
         alignSelf: 'flex-start',
